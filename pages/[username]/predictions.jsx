34d354628316ec23e4a3426481206d9783f53d8d
David McIntosh <dmcintosh@bblbze.com>
2022-04-13 12:38:25 -0600

    fixes more build issues.

--- pages/[username]/predictions.jsx
+++ pages/[username]/predictions.jsx
@@ -58,12 +58,12 @@ export default function PredictionsPage({ user, predictions }) {
             <AuthCheck>
                 <Metatags title="Predictions | Playoff Overtime" description="View or edit your playoff predictions." />
                 {predictions.length === 0 && (
-                    <section class="text-gray-600 body-font">
-                        <div class="container px-5 py-24 mx-auto">
-                            <div class="lg:w-2/3 flex flex-col sm:flex-row sm:items-center items-start mx-auto">
-                                <h1 class="flex-grow sm:pr-16 text-2xl font-medium title-font text-gray-900">Predict the 2022 NBA Playoffs.</h1>
+                    <section className="text-gray-600 body-font">
+                        <div className="container px-5 py-24 mx-auto">
+                            <div className="lg:w-2/3 flex flex-col sm:flex-row sm:items-center items-start mx-auto">
+                                <h1 className="flex-grow sm:pr-16 text-2xl font-medium title-font text-gray-900">Predict the 2022 NBA Playoffs.</h1>
                                 <Link href={`/${user.username}/predict`}>
-                                    <button class="flex-shrink-0 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg mt-10 sm:mt-0">Start</button>
+                                    <button className="flex-shrink-0 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg mt-10 sm:mt-0">Start</button>
                                 </Link>
                             </div>
                         </div>
--- pages/login.js
+++ pages/login.js
@@ -55,7 +55,7 @@ export default function Login(props) {
                             <Link href="/signup">
 
                                 <a className="text-indigo-500 inline-flex items-center mt-4">Sign Up
-                                    <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                    <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
                                         <path d="M5 12h14"></path>
                                         <path d="M12 5l7 7-7 7"></path>
                                     </svg>
